651daeab04b01d4bc63c91afe64e1d79ac048eb6
Jesus Perez <jesus@silentcircle-com>
2016-07-20 17:18:45 +0200

    Add system core patch when running envsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,10 @@
 add_lunch_combo cm_atenea-eng
 add_lunch_combo cm_atenea-user
 add_lunch_combo cm_atenea-userdebug
+
+cd system/core
+git checkout -f
+git clean -df
+git reset --hard HEAD 2&>1 >/dev/null
+git apply ../../device/hexxa/fenix/patches/system_core/fix-boot-for-mtk.patch
+cd ../..

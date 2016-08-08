e0a91c6a815de5b82c896289887cf78be25a200c
Jesus Perez <jesus@silentcircle-com>
2016-08-08 16:42:40 +0200

    Patch script fixed

--- omnipatches.sh
+++ omnipatches.sh
@@ -1,9 +1,9 @@
-cd $TOP_DIR/bionic
-patch -p1 < $TOP_DIR/device/hexxa/atenea/omnipatches/bionic/atenea.patch
-cd $TOP_DIR/external/sepolicy
-patch -p1 < $TOP_DIR/device/hexxa/atenea/omnipatches/external/sepolicy/atenea.patch
-cd $TOP_DIR/frameworks/base
-patch -p1 < $TOP_DIR/device/hexxa/atenea/omnipatches/frameworks/base/atenea.patch
-cd $TOP_DIR/system/core
-patch -p1 < $TOP_DIR/device/hexxa/atenea/omnipatches/system/core/atenea.patch
-cd $$TOP_DIR
+cd ../../../bionic
+patch -p1 < ../device/hexxa/atenea/omnipatches/bionic/atenea.patch
+cd ../external/sepolicy
+patch -p1 < ../../device/hexxa/atenea/omnipatches/external/sepolicy/atenea.patch
+cd ../../frameworks/base
+patch -p1 < ../../device/hexxa/atenea/omnipatches/frameworks/base/atenea.patch
+cd ../../system/core
+patch -p1 < ../../device/hexxa/atenea/omnipatches/system/core/atenea.patch
+cd ../../
